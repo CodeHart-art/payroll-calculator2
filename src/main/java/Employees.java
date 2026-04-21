9c20878c15153c1216c037ea835f6364a0157d39
CodeHart-art <jobhart11211@gmail.com>
2026-04-21 10:55:19 -0400

    grospay method created

--- src/main/java/Employees.java
+++ src/main/java/Employees.java
@@ -1,2 +1,35 @@
 public class Employees {
+    private int id;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+
+    public Employees(int id, String name, double hoursWorked, double payRate) {
+        this.id = id;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+    //region Start getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+    //endregion End getters
+
+    public double grossPay(double hoursWorked,double payRate){
+        return hoursWorked * payRate;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+    public static void main(String[] args) {
+
+        String filePath = "C:\\Users\\john1\\pluralsight\\workbook-3\\payroll-calculator\\src\\main\\resources\\employees.csv";
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+
+            String line;
+            bufferedReader.readLine();
+            while ((line = bufferedReader.readLine()) != null){
+
+                String[] parts = line.split("\\|");
+
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                double hours = Double.parseDouble(parts[2]);
+                double wage =Double.parseDouble(parts[3]);
+
+
+                Employees employees = new Employees(id,name,hours,wage);
+
+            }
+
+
+
+
+
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
